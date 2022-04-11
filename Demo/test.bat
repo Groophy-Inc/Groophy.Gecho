daaf826e0613d228b1ae30cf74c51bfb13db0c36
GroophyLifefor <77299279+GroophyLifefor@users.noreply.github.com>
2022-04-11 15:00:05 +0300

    Create test.bat

---
+++ Demo/test.bat
@@ -0,0 +1,7 @@
+@echo off
+call Gecho.exe "<r>red color"
+call Gecho.exe "<blue>blue color"
+call Gecho.exe "<gn>multi <magenta>color"
+call Gecho.exe "<dy>dark colors(darkyellow)"
+call Gecho.exe "<dgn>newline test<nl>hello bro(color saved)"
+pause
